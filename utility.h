0258e601e2f765af58c25071f1287bfbeff1d004
NNNILabs <osvivekanand002@gmail.com>
2023-03-22 18:49:31 +0100

    Update utility.h

--- utility.h
+++ utility.h
@@ -4,9 +4,8 @@
 float pioDivider = 100.0;
 int startPin = 0;
 
+void pioSetup(void);
 char get_on_bits(unsigned char byte);
 short random_bits(unsigned char no, unsigned long seed);
-
-void pioSetup(void);
 
 #endif
